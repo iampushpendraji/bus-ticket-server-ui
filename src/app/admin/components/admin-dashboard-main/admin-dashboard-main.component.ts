b3c23324412073e04c05fa16f1d7787cd4eae6d9
Pushpendra Sapkale <dev.psapkale@gmail.com>
2024-09-07 00:06:32 +0530

    Resolved cookies not getting stored in browser issue

--- src/app/admin/components/admin-dashboard-main/admin-dashboard-main.component.ts
+++ src/app/admin/components/admin-dashboard-main/admin-dashboard-main.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
+import { CommonNavComponent } from "../../../common/components/common-nav/common-nav.component";
+
 
 @Component({
   selector: 'app-dashboard-main',
   standalone: true,
-  imports: [],
+  imports: [CommonNavComponent],
   templateUrl: './admin-dashboard-main.component.html',
   styleUrl: './admin-dashboard-main.component.css'
 })
+
+
 export class AdminDashboardMainComponent {
 
 }
